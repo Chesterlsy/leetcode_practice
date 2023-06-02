35ab33ae9f56e4ed70a8f913f2c3c65b76481189
Chesterlsy <siyiliu641@hotmail.com>
2023-06-03 05:10:47 +0800

    Fixed pulverizer for negative numbers Added pulverizer for positive s

--- src/com/chester/leetcode_practice/algorithms/numbertheory/Pulverizer.java
+++ src/com/chester/leetcode_practice/algorithms/numbertheory/Pulverizer.java
@@ -9,6 +9,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class Pulverizer {
+
+    /*
+    gcd(a, b) = s*a + t*b
+     */
+    public static int[] pulverizer(int a, int b) {
+        return pulverizer(a, b, new int[]{1, 0}, new int[]{0, 1});
+    }
+
     /*
     x = s1*a + t1*b
     y = s2*a + t2*b
@@ -30,14 +38,51 @@ class Pulverizer {
                     br,
                     new int[]{ar[0] - q * br[0], (ar[1] - q * br[1])});
         }
+        // x < 0
+        if (result[0] * a + result[1] * b < 0) {
+            result[0] *= -1;
+            result[1] *= -1;
+        }
+        return result;
+    }
+
+    public static int[] pulverizerPositiveS(int a, int b) {
+        int[] result = pulverizer(a, b);
+        int s = result[0];
+        if (a != 0 && b != 0 && s < 0) {
+            // (s + k*b)*a + (t - k*a)*b
+            // k*b + s > 0
+            int k;
+            if (b > 0) { // k > -s/b
+                k = -s / b + 1;
+            } else { // k < -s/b
+                k = -s / b - 1;
+            }
+            result[0] += k * b;
+            result[1] += -k * a;
+        }
         return result;
     }
 
     @Test
     void test() {
-        int[] actual = pulverizer(899, 493, new int[]{1, 0}, new int[]{0, 1});
-        int[] expect = {-6, 11};
-        assertArrayEquals(expect, actual);
+        assertArrayEquals(new int[]{-6, 11}, pulverizer(899, 493));
+        assertArrayEquals(new int[]{11, -6}, pulverizer(493, 899));
+        assertArrayEquals(new int[]{6, 11}, pulverizer(-899, 493));
+        assertArrayEquals(new int[]{-6, -11}, pulverizer(899, -493));
+        assertArrayEquals(new int[]{6, -11}, pulverizer(-899, -493));
+        assertArrayEquals(new int[]{1, 0}, pulverizer(899, 0));
+        assertArrayEquals(new int[]{0, 1}, pulverizer(0, 493));
+    }
+
+    @Test
+    void testPositiveS() {
+        assertArrayEquals(new int[]{487, -888}, pulverizerPositiveS(899, 493));
+        assertArrayEquals(new int[]{6, 11}, pulverizerPositiveS(-899, 493));
+        assertArrayEquals(new int[]{487, 888}, pulverizerPositiveS(899, -493));
+        assertArrayEquals(new int[]{6, -11}, pulverizerPositiveS(-899, -493));
+        assertArrayEquals(new int[]{1, 0}, pulverizerPositiveS(899, 0));
+        assertArrayEquals(new int[]{0, 1}, pulverizerPositiveS(0, 493));
     }
 
 }
